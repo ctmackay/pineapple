ab344130abed9c4d98ab8b22af9e617adc47f3eb
Cherry <chrryy@gmail.com>
2017-11-16 20:07:28 +0100

    oops forgot the most important remote file

---
+++ main_controller/scripts/remote.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from main_controller.msg import *
+import rospy
+
+def remote_server():
+    pub = rospy.Publisher('remote', Remote, queue_size = 10)
+    rospy.init_node('remote_server', anonymous=True)
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        remote_output = Remote()
+        rospy.loginfo('%s' % remote_output)
+        pub.publish(remote_output)
+        rate.sleep()
+
+if __name__ == "__main__":
+    try:
+        remote_server()
+    except rospy.ROSInterruptException:
+        pass
