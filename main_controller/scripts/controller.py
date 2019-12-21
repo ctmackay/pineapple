e06efef97934f00e5d4c57d0c75088fcf2ab7c92
sammy <sammy@nightingalesystem.com>
2019-12-20 17:42:20 -0800

    modify controller file to delete robot commands for now, add bash scripts to start pineapple

--- main_controller/scripts/controller.py
+++ main_controller/scripts/controller.py
@@ -8,66 +8,36 @@ import time
 
 g_remote_output = None
 
-# define state Sleep 
 class Sleep(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['waking up', 'stay asleep'])
 
     def execute(self, userdata):
-        rospy.loginfo('Executing state Sleep')
         time.sleep(3)
+        return 'stay asleep'
 
-        if not g_remote_output == None:
-            if g_remote_output.awake:
-                rospy.loginfo('Wake up')
-                return 'waking up'
-            else:
-                rospy.loginfo('Stay asleep')
-                return 'stay asleep'
-        else:
-            return 'stay asleep'
 
-# define state Awake
 class Awake(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['go to sleep', 'stay awake'])
 
     def execute(self, userdata):
-        global g_remote_output
-
-        rospy.loginfo('Executing state Awake')
         time.sleep(3)
+        return 'stay awake'
 
-        rospy.loginfo('remote output %s' % g_remote_output)
-        if not g_remote_output == None:
-            if g_remote_output.sleep:
-                rospy.loginfo('Go to sleep')
-                return 'go to sleep'
-            else:
-                rospy.loginfo('Stay awake')
-                return 'stay awake'
-        else:
-            return 'stay awake'
-
-        #rospy.loginfo('g_receive_sleep_command %s' % sleep_command)
-        #if sleep_command:
-        #    rospy.loginfo('Go to sleep')
-        #    return 'sleeping'
-        #else:
-        #    rospy.loginfo('Stay awake')
-        #    return 'stay awake'
 
 def callback(msg):
     global g_remote_output
     g_remote_output = msg
 
-# main
+
 def main():
     rospy.init_node('smach_example_state_machine')
 
     # Create a SMACH state machine
     sm = smach.StateMachine(outcomes=['waking up'])
 
+    # Subscribers
     rospy.Subscriber('/remote', Remote, callback)
 
     # Open the container
